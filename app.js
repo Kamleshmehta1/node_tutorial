52f030b0a64998c538f8ae70d943241d30dfe059
kamlesh <kamleshm1999@gmail.com>
2023-04-12 23:25:30 +0530

    18th april

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+//import NPM modules
+
 const express = require('express')
-const cleanUpAndValidate = require('./utils/AuthUtils')
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
+const validator = require('validator')
+const session = require('express-session')
+const mongoDbSession = require('connect-mongodb-session')(session);
+
+// import utils
+
+const { cleanUpAndValidate } = require('./utils/AuthUtils')
 const dataBase = require('./private-constants')
+
+// import userModels
+
+const UserModel = require('./model/UserModel')
 
 
 const app = express()
+
+
 
 const PORT = 4000
 
@@ -17,8 +31,24 @@ mongoose.connect(dataBase.mongoURI, {
     console.log('connected to database')
 })
 
+
+const dataBaseSession = new mongoDbSession({
+    uri: dataBase.mongoURI,
+    collection: 'mySession'
+})
+
+app.use(
+    session({
+        secret: "Our Secret Key",
+        resave: true,
+        saveUninitialized: true,
+        store: dataBaseSession
+    })
+);
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+
 
 app.set('view engine', 'ejs')
 
@@ -34,31 +64,130 @@ app.get('/register', (req, resp) => {
 
 
 app.post('/login', async (req, resp) => {
+    const { loginId, password } = req.body
+    console.log(loginId, password);
+
+    let user;
+
+    if (validator.isEmail(loginId)) {
+        // loginId is email
+        user = await UserModel.findOne({ email: loginId })
+    } else {
+        //loginId is userName
+        user = await UserModel.findOne({ userName: loginId })
+
+        // user not found
+
+        if (!user) {
+            return resp.send({
+                status: 401,
+                message: "Data not found",
+                data: req.body
+            })
+        }
+
+        const isMatch = bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return resp.send({
+                status: 401,
+                message: "Wrong password",
+                data: req.body
+            })
+        }
+
+        console.log(req.session)
+
+        req.session.isAuth = true;
+        req.session.user = { email: user.email, userName: user.userName }
+        return resp.redirect('/home')
+
+        // return resp.send({
+        //     status: 200,
+        //     message: "Login successfull !",
+        //     data: {
+        //         name: user.name,
+        //         email: user.email,
+        //         userName: user.userName
+        //     }
+        // })
+    }
+    try {
+        await cleanUpAndValidate({ name, email, password, userName })
+    } catch (error) {
+        return resp.send({ status: 400, message: "invalid data", data: error })
+    }
+})
+
+
+
+app.get('/home', (req, resp) => {
+
+    if (req.session.isAuth) {
+        return resp.send('Welcome to the home page')
+    } else {
+        return resp.send('Invalid session')
+    }
+})
+
+app.post('/register', async (req, resp) => {
     const { name, email, password, userName } = req.body
-    console.log(req.body)
-
-
+    console.log(req.body);
     try {
         await cleanUpAndValidate({ name, email, password, userName })
     } catch (error) {
         return resp.send({ status: 400, message: "invalid data", data: error })
     }
 
-})
-app.post('/register', async (req, resp) => {
-    const { name, email, password, userName } = req.body
-    console.log(req.body)
-    resp.send(req.body)
+    // check if user already exist
     try {
-        await cleanUpAndValidate({ name, email, password, userName })
+        const emailId = await UserModel.findOne({ email });
+        const user = await UserModel.findOne({ userName });
+
+        if (user) {
+            return resp.send({
+                status: "400",
+                message: "userName already exist",
+            })
+        }
+        if (emailId) {
+            return resp.send({
+                status: "400",
+                message: "email already exist",
+            })
+        }
     } catch (error) {
-        return resp.send({ status: 400, message: "invalid data", data: error })
+        return resp.send({
+            status: "400",
+            message: "Database error",
+            data: error
+        })
     }
 
     // password encryption
     const hashedPassword = await bcrypt.hash(password, 13)
 
     //  store the data in data base
+
+    const user = new UserModel({
+        name, email, userName, password: hashedPassword
+    })
+
+    try {
+        const res = await user.save();
+
+        return resp.send({
+            status: 200,
+            message: "registered successfully!",
+            data: res
+        })
+    } catch (error) {
+        return resp.send({
+            status: 400,
+            message: "error",
+            data: error
+        })
+    }
 
 })
 
--- utils/AuthUtils.js
+++ utils/AuthUtils.js
@@ -2,34 +2,45 @@ const validator = require('validator')
 
 const cleanUpAndValidate = async ({ name, email, password, userName }) => {
     return new Promise((resolve, reject) => {
-        if (typeof email !== 'string' && email !== 'string' && password !== 'string' && userName !== 'string') {
-            reject('Invalid Email')
+        if (typeof (email) != 'string') {
+            reject('Invalid email')
+        }
+        if (typeof (name) != 'string') {
+            reject('Invalid name')
+        }
+        if (typeof (userName) != 'string') {
+            reject('Invalid userName')
+        }
+        if (typeof (password) != 'string') {
+            reject('Invalid password')
         }
 
+
         if (!email || !password || !userName) {
-            reject("Invalid data")
+            reject("Invalid email || password || userName")
         }
 
         if (!validator.isEmail(email)) {
             reject('Invalid email')
         }
 
-        if (userName.toString().length < 3) {
+        if (userName.length < 3) {
             reject('username length too short !')
         }
-        if (userName.toString().length > 50) {
+        if (userName.length > 50) {
             reject('username length too long !')
         }
-        if (password.toString().length > 5) {
+        if (password.length < 5) {
             reject('password length too short !')
         }
-        if (password.toString().length > 200) {
+        if (password.length > 200) {
             reject('password length too long !')
         }
+
 
         resolve()
     })
 }
 
 
-module.exports = { cleanUpAndValidate }
+module.exports = { cleanUpAndValidate } 
